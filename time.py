fc1da5e5154d578419098c948080a3ad7a296491
Qiushi <1530875862@qq.com>
2020-12-27 18:02:44 +0800

    fix time and order

--- time.py
+++ time.py
@@ -1,4 +1,13 @@
 import datetime
+import time
 
-def get_time_now():
+def get_time_now(): #day h min and sec
     cur_time=datetime.datetime.now()
+    return cur_time
+
+def get_time_stamp(): #get timestamp for auto cancelling orders that are not paid in time
+    cur_time_stamp = time.time()
+    return cur_time_stamp
+
+# timestamp = get_time_stamp()
+# print(timestamp)
