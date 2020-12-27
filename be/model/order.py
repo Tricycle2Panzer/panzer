f5ab76c151a96ec1e3c4aa1c01e92bd0be6d456d
Qiushi <1530875862@qq.com>
2020-12-27 19:14:00 +0800

    add order.py

--- be/model/order.py
+++ be/model/order.py
@@ -1,6 +1,7 @@
 import datetime
 from be.model.timer import get_time_stamp
 from be.model.timer import get_time_now
+time_limit=30
 daemon = 0
 to_be_paid = [[daemon,get_time_stamp()]]
 # to_be_paid.append([daemon2,get_time_stamp()])
@@ -16,4 +17,11 @@ def delete_pending_order(orderID):
         if to_be_paid[i][0]==orderID:
             to_be_paid.pop(i) #will return something
             break
+    return 0
+
+def time_exceed_delete():
+    cur_time = get_time_stamp()
+    for i in range(0,len(to_be_paid)):
+        if cur_time-to_be_paid[i][1]>time_limit:
+            delete_pending_order(i)
     return 0
