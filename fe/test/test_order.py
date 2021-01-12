ddebc85bd3aafb3a3410a7e9e694d26479c82802
AvivaTang <10182100106@stu.ecnu.edu.cn>
2021-01-12 18:55:42 +0800

    update workload & pytest
    update workload & pytest

--- fe/test/test_order.py
+++ fe/test/test_order.py
@@ -34,10 +34,18 @@ class TestSendBooks:
     # def test_non_exist_order_id(self):
     #
 
+    def test_status_error(self):
+        ok, buy_book_id_list = self.gen_book.gen(non_exist_book_id=False, low_stock_level=False)
+        assert ok
+        code0, order_id = self.buyer.new_order(self.store_id, buy_book_id_list)
+        code = self.seller.send_books(self.store_id,order_id)
+        assert code != 200
+
     def test_send_books_ok(self):
         ok, buy_book_id_list = self.gen_book.gen(non_exist_book_id=False, low_stock_level=False)
         assert ok
         code0, order_id = self.buyer.new_order(self.store_id, buy_book_id_list)
+        code = self.buyer.payment(order_id)
         code = self.seller.send_books(self.store_id,order_id)
         assert code == 200
 
